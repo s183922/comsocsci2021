03bc54c181c45121f7534a61fe3df4a8a896a685
Peter Grønning <s183922@win.dtu.dk>
2021-02-09 12:11:29 +0100

    stuff

--- lectures/download.py
+++ lectures/download.py
@@ -1,42 +1,35 @@
-import pandas as pd
-from threading import Thread
-import numpy as np
+### GET COMMENTS ###
+from tqdm import tqdm
 from psaw import PushshiftAPI
-
-
-api = PushshiftAPI()
+import pandas as pd
+from datetime import datetime
 df = pd.read_csv("reddit.csv")
+api = PushshiftAPI()
 
+sub_id = list(df["id"][df["num_comments"]> 0])
+N = 400
+steps = np.linspace(0, len(sub_id)-1, N, dtype = np.int)
 
-def download_comments(ids, data):
+date1 = int(datetime(2020,1,1).timestamp())
+date2 = int(datetime(2021,1,25).timestamp())
+subreddit = 'WallStreetBets'
+query = "GME|Gamestop"
+
+comments = []
+for i in tqdm(range(1,N)):
+    ids = sub_id[steps[i-1]:steps[i]]
+    gen_comments = api.search_comments(subreddit = subreddit,
+                                      after = date1,
+                                      before = date2,
+                                      link_id = ids)
+    comments.extend(list(gen_comments))
     
-    for id in ids:
-        gen =  api.search_comments(link_id = str(id))
-        results = list(gen)
-        data.extend([(p.d_["id"], str(id), p.d_["score"], p.d_["created_utc"], p.d_["author"], p.d_["parent_id"]) for p in results])
-    return data
-
-ids = df["id"][df["num_comments"] > 0]
-
-threads = 5
-steps = np.linspace(0,len(ids), threads, dtype=np.int)
-jobs = []
-data = []
-
-
-for i in range(1, threads):
-    
-    thread = Thread(target=download_comments(ids[steps[i-1]:steps[i]], data))
-    jobs.append(thread)
-    
-    
-for i, j in enumerate(jobs):
-    print("Running job ", i)
-    j.start()
-    
-for j in jobs:
-    j.join()
-
+data = [(p.d_["id"],
+         p.d_["link_id"],
+         p.d_["score"],
+         p.d_["created_utc"],
+         p.d_["author"],
+         p.d_["parent_id"]) for p in comments]
 columns = ["id", "submission_id", "score", "creation_date", "author", "parent_id"]
-df_columns = pd.DataFrame(data, columns = columns)
-df_columns.to_csv("comments.csv", index = False)
+df_comments = pd.DataFrame(data, columns = columns)
+df_comments.to_csv("comments.csv", index = False)
